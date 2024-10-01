edb142fa97596141e1229e830d8f17ff31d0e180
RAUL ANTÔNIO DA SILVEIRA <632220012@fspoa.br>
2024-09-30 21:41:20 -0300

    commit de fim de aula

--- TemDeTudo/Controllers/SellersController.cs
+++ TemDeTudo/Controllers/SellersController.cs
@@ -1,12 +1,50 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TemDeTudo.Data;
+using TemDeTudo.Models;
 
 namespace TemDeTudo.Controllers
 {
     public class SellersController : Controller
     {
-       public IActionResult Index()
+        private readonly TemDeTudoContext _context;
+        
+        public SellersController(TemDeTudoContext context)
+        {
+            _context = context;
+        }
+        
+        public IActionResult Index()
+        {
+            //List<Seller> sellers = _context.Seller.ToList();
+            var sellers = _context.Seller.Include("Department").ToList();
+            return View(sellers);
+        }
+
+        public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Seller seller)
+        {
+            //Testa se foi passado um vendedor
+            if (seller == null)
+            {
+               //Retorna página não encontrada 
+               return NotFound();
+            }
+
+            seller.Department = _context.Department.FirstOrDefault();
+            seller.DepartmentId = seller.Department.Id;
+
+            //Adicionar o objeto vendedor ao banco
+            _context.Add(seller);
+            //Confirma as alterações no bd
+            _context.SaveChanges();
+            
+            return RedirectToAction("Index");
         }
     }
 }
